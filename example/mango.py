b7aa3f1f77b35c13287465be519979d57dea9ce2
Frederic Boudon <frederic.boudon@cirad.fr>
2017-10-09 14:06:50 +0200

    check light module

--- example/mango.py
+++ example/mango.py
@@ -4,13 +4,27 @@
 import pandas
 import openalea.plantgl.all as pgl
 from alinea.astk.sun_and_sky import sun_sky_sources
-from alinea.caribu.CaribuScene import CaribuScene
-from alinea.caribu.light import light_sources
-from alinea.pyratp.interface.ratp_scene import RatpScene
+try:
+    from alinea.caribu.CaribuScene import CaribuScene
+    from alinea.caribu.light import light_sources
+    with_caribu = True
+except ImportError, ie:
+    with_caribu = False
+
+try:
+    from vplants.fractalysis.light.directLight import scene_irradiance
+    with_muslim = True
+except ImportError, ie:
+    with_muslim = False
+
+try:
+    from alinea.pyratp.interface.ratp_scene import RatpScene
+    with_ratp = True
+except ImportError, ie:
+    with_ratp = False
+
+
 import time
-
-
-from vplants.fractalysis.light.directLight import scene_irradiance
 
 def reader(data_file='rayostpierre2002.csv'):
     """ reader for mango meteo files """
@@ -37,46 +51,49 @@ localisation={'latitude':-21.32, 'longitude':55.5, 'timezone': 'Indian/Reunion'}
 
 meteo = reader()
 
-dates = pandas.date_range(start='2002-09-02', end = '2002-09-15', freq='H',tz='Indian/Reunion')
+dates = pandas.date_range(start='2002-09-02', end = '2002-09-03', freq='H',tz='Indian/Reunion')
 ghi = meteo.loc[dates,'global_radiation']
 
 sun, sky = sun_sky_sources(ghi=ghi, dates=dates, **localisation)
 
 # Caribu
-
-print 'start caribu...'
-t = time.time()
-light = light_sources(*sun) + light_sources(*sky)
-cs = CaribuScene(mango, light=light, scene_unit='cm')
-raw, agg = cs.run(direct=True, simplify=True)
-res = pandas.DataFrame(agg)
-print 'made in', time.time() - t
+if with_caribu:
+    print 'start caribu...'
+    t = time.time()
+    light = light_sources(*sun) + light_sources(*sky)
+    cs = CaribuScene(mango, light=light, scene_unit='cm')
+    raw, agg = cs.run(direct=True, simplify=True)
+    res = pandas.DataFrame(agg)
+    print 'made in', time.time() - t
 
 # #muslim
-print 'start muslim...'
-t = time.time()
-# permute az, el, irr to el, az, irr
-sun_m = sun[1], sun[0], sun[2]
-sky_m = sky[1], sky[0], sky[2]
-directions = zip(*sun_m) + zip(*sky_m)
-resm =scene_irradiance(mango, directions, horizontal=True, scene_unit='cm')
-print 'made in', time.time() - t
+if with_muslim:
+    print 'start muslim...'
+    t = time.time()
+    # permute az, el, irr to el, az, irr
+    sun_m = sun[1], sun[0], sun[2]
+    sky_m = sky[1], sky[0], sky[2]
+    directions = zip(*sun_m) + zip(*sky_m)
+    defm =scene_irradiance(mango, directions, horizontal=True, scene_unit='cm', screenwidth = 1000)
+    print 'made in', time.time() - t
 
 
 #ratp
-print 'start ratp...'
-t = time.time()
-ratp = RatpScene(mango, rleaf=0, rsoil=0, scene_unit='cm', resolution=(0.15, 0.15, 0.15), mu=0.7)
-dfv = ratp.do_irradiation(sky_sources=sky, sun_sources=sun, mu=0.6)
-resr = ratp.scene_lightmap(dfv, 'shape_id')
-dfr = resr.loc[:,('shape_id', 'Area', 'PAR')].set_index('shape_id')
-print 'made in', time.time() - t
+if with_ratp:
+    print 'start ratp...'
+    t = time.time()
+    ratp = RatpScene(mango, rleaf=0, rsoil=0, scene_unit='cm', resolution=(0.15, 0.15, 0.15), mu=0.7)
+    dfv = ratp.do_irradiation(sky_sources=sky, sun_sources=sun, mu=0.6)
+    resr = ratp.scene_lightmap(dfv, 'shape_id')
+    dfr = resr.loc[:,('shape_id', 'Area', 'PAR')].set_index('shape_id')
+    print 'made in', time.time() - t
 
 # compare
 res = res.rename(columns={'area': 'area_c'})
-df = res.join(resm).join(dfr)
-df.plot('irradiance', ['Ei', 'PAR'],xlim=(0,35000), ylim=(0,35000), style='p')
-df.plot('area', ['area_c', 'Area'],xlim=(0,0.2), ylim=(0,0.2), style='p')
+if with_ratp:
+    df = res.join(resm).join(dfr)
+    df.plot('irradiance', ['Ei', 'PAR'],xlim=(0,35000), ylim=(0,35000), style='p')
+    df.plot('area', ['area_c', 'Area'],xlim=(0,0.2), ylim=(0,0.2), style='p')
 
 
 
